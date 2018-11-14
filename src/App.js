3000ea40d312c04e63f96ea9ce52db19559bc550
Shayna Justus <shayna.justus@gmail.com>
2018-11-14 07:15:01 -0800

    Clean up MainPage component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import MainPage from './MainPage'
 
 class BooksApp extends React.Component {
 
   render() {
     return (
       <div className="app">
-
+        <MainPage />
       </div>
     )
   }
--- src/MainPage.js
+++ src/MainPage.js
@@ -1,24 +1,24 @@
+import React from 'react'
 
+class MainPage extends React.Component {
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
 
-<div className="list-books">
-  <div className="list-books-title">
-    <h1>MyReads</h1>
-  </div>
+        <div className="list-books-content">
+          <div>
 
-  <div className="list-books-content">
-    <div>      
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">Want to Read</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-
-          </ol>
+          </div>
+        </div>
+        <div className="open-search">
+          <a>Add a book</a>
         </div>
       </div>
-    </div>
-  </div>
+    )
+  }
+}
 
-  <div className="open-search">
-    <a>Add a book</a>
-  </div>
-</div>
+export default MainPage
