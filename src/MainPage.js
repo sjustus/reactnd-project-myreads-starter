0b8bdd83a08ec77004a9ea184efa47d76952e5b7
Shayna Justus <shayna.justus@gmail.com>
2018-11-14 06:51:34 -0800

    Break starter code into components

---
+++ src/MainPage.js
@@ -0,0 +1,24 @@
+
+
+<div className="list-books">
+  <div className="list-books-title">
+    <h1>MyReads</h1>
+  </div>
+
+  <div className="list-books-content">
+    <div>      
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Want to Read</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+
+          </ol>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div className="open-search">
+    <a>Add a book</a>
+  </div>
+</div>
