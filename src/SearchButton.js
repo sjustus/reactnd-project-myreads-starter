3b39a4e4c76343f7347a0a710f7f09fee478897f
Shayna Justus <shayna.justus@gmail.com>
2018-11-15 06:50:47 -0800

    Add Link to SearchButton

--- src/SearchButton.js
+++ src/SearchButton.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class SearchButton extends React.Component {
   render() {
     return (
       <div className="open-search">
-        <a>Add a book</a>
+        <Link to='/search'>Add a book</Link>
       </div>
     )
   }
