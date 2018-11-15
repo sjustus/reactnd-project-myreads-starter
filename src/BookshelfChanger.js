99bd31c3cf833c396fc1956023d56cf1073ef45c
Shayna Justus <shayna.justus@gmail.com>
2018-11-15 07:26:47 -0800

    Extract BookshelfChanger from Book
    To clean up code and make it easier for me to read

--- src/BookshelfChanger.js
+++ src/BookshelfChanger.js
@@ -1,1 +1,17 @@
+import React from 'react'
 
+class BookshelfChanger extends React.Component {
+  render() {
+    return (
+      <select>
+        <option value="move" disabled>Move to...</option>
+        <option value="currentlyReading">Currently Reading</option>
+        <option value="wantToRead">Want to Read</option>
+        <option value="read">Read</option>
+        <option value="none">None</option>
+      </select>
+    )
+  }
+}
+
+export default BookshelfChanger
