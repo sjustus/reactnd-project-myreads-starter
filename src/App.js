e30bfec7618ae6be4f81b38bac9f8b41bb26eaff
Shayna Justus <shayna.justus@gmail.com>
2018-11-15 20:40:57 -0800

    Add function to get books from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import MainPage from './MainPage'
 
 class BooksApp extends React.Component {
+  constructor(props) {
+    super(props);
+    // set state of books in App to pass to MainPage and SearchPage
+    this.state = {
+      books: [],
+    }
+  }
+
+  // once the component mounts
+  componentDidMount() {
+    // get books from the BooksAPI - returns an array of books
+    BooksAPI.getAll()
+    // set the state of books to response & console.log books
+    // Reference: https://reactjs.org/docs/state-and-lifecycle.html & Udacity videos (React)
+    .then((resp) => {
+      this.setState({
+        books: resp
+      });
+      console.log(this.state.books);
+    }); // then
+
+
+  }
 
   render() {
     return (
