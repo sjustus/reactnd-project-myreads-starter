cf755d07523e7bff646e3d60efc2e1a8fb0af4d6
Shayna Justus <shayna.justus@gmail.com>
2018-11-17 20:05:05 -0800

    Refactor Book to dynamically render details

--- src/Book.js
+++ src/Book.js
@@ -4,17 +4,25 @@ import BookshelfChanger from './BookshelfChanger'
 class Book extends React.Component {
 
   render() {
+    console.log('Props', this.props);
+
     return (
       <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+          {/*
+            *imageLinks are object w/ props that contain urls - use template literal to grab url as string *
+            *w/help form Camille B. on Slack helped me figure that I need a check first
+            *check for imageLinks object, if imageLinks, get thumbnail, else display "No image"
+          */}
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : 'No image available'})` }}></div>
             <div className="book-shelf-changer">
               <BookshelfChanger />
             </div>
           </div>
-          <div className="book-title">To Kill a Mockingbird</div>
-          <div className="book-authors">Harper Lee</div>
+          <div className="book-title">{this.props.book.title}</div>
+          {/* Authors prop is array - 1st check if author, if yes display first, if no display 'No author...'*/}
+          <div className="book-authors">{this.props.book.authors[0] ? this.props.book.authors[0] : 'No author available'}</div>
         </div>
       </li>
     )
