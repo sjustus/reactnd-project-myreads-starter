f51124fe9e15fd3fef956bb9e18d79d0a8b2114d
Shayna Justus <shayna.justus@gmail.com>
2018-11-17 20:34:21 -0800

    @feat: Main page displays books by shelf

--- src/MainPage.js
+++ src/MainPage.js
@@ -13,9 +13,13 @@ class MainPage extends React.Component {
 
         <div className="list-books-content">
           <div>
-            <Bookshelf shelf="Currently Reading" books={this.props.books}/>
-            <Bookshelf shelf="Want to Read" books={this.props.books} />
-            <Bookshelf shelf="Read" books={this.props.books}/>
+          {/*
+            *Filter through books on shelves to display books with matching shelf property
+            *For help w/ filter syntax (article): 'Array, Map, Filter, and Reduce in JS by John Ferris '[https://atendesigngroup.com/blog/array-map-filter-and-reduce-js]
+          */}
+            <Bookshelf shelf="Currently Reading" books={this.props.books.filter((book) => (book.shelf === "currentlyReading"))}/>
+            <Bookshelf shelf="Want to Read" books={this.props.books.filter((book) => (book.shelf === "wantToRead"))} />
+            <Bookshelf shelf="Read" books={this.props.books.filter((book) => (book.shelf === "read"))}/>
           </div>
         </div>
 
