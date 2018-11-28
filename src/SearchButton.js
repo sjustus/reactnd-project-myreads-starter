38869740872f8197272a33a5bb6ff5457572bfbc
Shayna Justus <shayna.justus@gmail.com>
2018-11-27 21:03:24 -0800

    Convert SearchButton to stateless functional component

--- src/SearchButton.js
+++ src/SearchButton.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-class SearchButton extends React.Component {
-  render() {
-    return (
-      <div className="open-search">
-        <Link to='/search'>Add a book</Link>
-      </div>
-    )
-  }
+/*
+  * reviewer suggestion -- convert to stateless functional component since component does not use state
+*/
+
+const SearchButton = ({searchButton}) => {
+  return (
+    <div className="open-search">
+      <Link to='/search'>Add a book</Link>
+    </div>
+  )
 }
+
 
 export default SearchButton
